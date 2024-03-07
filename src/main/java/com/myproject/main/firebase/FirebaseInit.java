25d11ea9e1c2cade612f7d2b3fa1a7bfa494812a
NTuan281 <113279374+NTuan281@users.noreply.github.com>
2024-03-07 20:18:25 +0700

    update firebase

--- src/main/java/com/myproject/main/firebase/FirebaseInit.java
+++ src/main/java/com/myproject/main/firebase/FirebaseInit.java
@@ -17,23 +17,35 @@ import com.google.firebase.database.FirebaseDatabase;
 @Service
 public class FirebaseInit {
 
-	FirebaseDatabase db;
-
-    public FirebaseInit() throws IOException {
-    	InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
+//	FirebaseDatabase db;
+//
+//    public FirebaseInit() throws IOException {
+//    	InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
+//
+//        FirebaseOptions options = new FirebaseOptions.Builder()
+//                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                .setDatabaseUrl("https://learncode-67c4d-default-rtdb.asia-southeast1.firebasedatabase.app")
+//                .build();
+//
+//        FirebaseApp.initializeApp(options);
+//
+//        db = FirebaseDatabase.getInstance();
+//    }
+//
+//    public FirebaseDatabase getDb() {
+//        return db;
+//    }
+	
+	public static void initialize() throws Exception {
+        FileInputStream serviceAccount =
+            new FileInputStream("main/resources/serviceAccountKey.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("https://learncode-67c4d-default-rtdb.asia-southeast1.firebasedatabase.app")
-                .build();
+            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+            .setDatabaseUrl("https://learncode-67c4d-default-rtdb.asia-southeast1.firebasedatabase.app")
+            .build();
 
         FirebaseApp.initializeApp(options);
-
-        db = FirebaseDatabase.getInstance();
-    }
-
-    public FirebaseDatabase getDb() {
-        return db;
     }
 
 }
